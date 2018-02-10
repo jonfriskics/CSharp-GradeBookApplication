4e56b9502a189c9e63f7e30a52b0d815560bb0ba
Jon Friskics <jonfriskics@gmail.com>
2018-02-10 15:22:28 -0500

    add grade ranks

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook: BaseGradeBook
@@ -12,6 +14,18 @@ namespace GradeBook.GradeBooks
             if (this.Students.Count < 5) {
                 throw new InvalidOperationException();
             }
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[threshold - 1])
+                return 'A';
+            if (averageGrade >= grades[(threshold * 2) - 1])
+                return 'B';
+            if (averageGrade >= grades[(threshold * 3) - 1])
+                return 'C';
+            if (averageGrade >= grades[(threshold * 4) - 1])
+                return 'D';
             return 'F';
         }
     }
